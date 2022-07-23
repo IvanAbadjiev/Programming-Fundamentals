f9a0f0f1a96a3ecb99999b6aca8229952a480715
IvanAbadjiev <ivan087611@gmail.com>
2022-07-23 21:45:19 +0300

    Exercise

---
+++ Data Types and Variables/Exercise/07. Water Overflow/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _07._Water_Overflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n = byte.Parse(Console.ReadLine());
+            ushort capacity = 255;
+            ushort sumLiter = 0;
+            for (int i = 0; i < n; i++)
+            {
+                ushort curentLiteerAdd = ushort.Parse(Console.ReadLine());
+
+                if (curentLiteerAdd <= capacity)
+                {
+                    sumLiter += curentLiteerAdd;
+                    capacity -= curentLiteerAdd;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+
+
+
+            }
+            Console.WriteLine(sumLiter);
+
+        }
+    }
+}
---
+++ Data Types and Variables/Exercise/08. Beer Kegs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _08._Beer_Kegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n = byte.Parse(Console.ReadLine());
+
+            string lastBigKag = string.Empty;
+            double lastVolumKag = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string modelKag = Console.ReadLine();
+                double radiusKag = double.Parse(Console.ReadLine());
+                uint heightKag = uint.Parse(Console.ReadLine());
+
+                double volume = Math.PI * Math.Pow(radiusKag, 2) * heightKag;
+
+                if (lastVolumKag < volume)
+                {
+                    lastVolumKag = volume;
+                    lastBigKag = modelKag;
+                }
+
+
+
+            }
+            Console.WriteLine(lastBigKag);
+        }
+    }
+}
